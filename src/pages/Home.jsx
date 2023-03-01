4602b6d69ef9b96efdd9bb53c5678e5dcf60f52d
Sylvain <sy.caparros@laposte.net>
2023-03-01 23:24:27 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
+import '../style/App.css';
 
 function App() {
+  console.log('salut')
   return (
     <div className="App">
       <header className="App-header">
