7cc8499e38db192d5cc053e5b05e4a2c1fb2619b
Sylvain <sy.caparros@laposte.net>
2023-03-08 01:19:05 +0100

    Dropdown

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import '../../sass/dropdown.scss'
+
+export default function Dropdown({ bar, description }) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div>
+            <div className='bar' onClick={() => setIsOpen(!isOpen)}>
+                <div className='bar_title'>{ bar }</div>
+                <div className={'bar_arrow' + ( isOpen ? '' : ' bar_arrow_down' )}>
+                    <FontAwesomeIcon icon={ faAngleUp } />
+                </div>
+            </div>
+            { isOpen 
+                ? <div className='description'>
+                    { Array.isArray(description)
+                        ? description.map(e => <div>{e}</div>)
+                        : description
+                    }
+                  </div>
+                : null
+            }
+        </div>
+    )
+}
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,9 +1,21 @@
+import Dropdown from '../components/Dropdown/Dropdown'
+import Apts from '../datas/apts.json'
 import '../sass/Housing.scss'
 
-export default function Housing(props) {
+let url = document.location.href
+let idurl = new URL(url)
+let id = idurl.pathname.replace('/housing/', '')
+const aptFound = Apts.find(e => e.id === id)
+
+export default function Housing() {
     return (
-      <div className="Housing">
-        <h2>Housing</h2>
-      </div>
+      <main className="Housing">
+        <div className='Housing_col'>
+          <Dropdown bar='Equipements' description={aptFound.equipments} />
+        </div>
+        <div className='Housing_col'>
+          <Dropdown bar='Equipements' description={aptFound.equipments} />
+        </div>
+      </main>
     );
   }
