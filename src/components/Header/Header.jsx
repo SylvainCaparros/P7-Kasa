a22fe2a76701672ab0902c58a772ffdf5eb6afd7
Sylvain <sy.caparros@laposte.net>
2023-03-15 09:37:09 +0100

    Modif menu en responsive/ responsive page 404

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,25 @@
 import '../../sass/header.scss'
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom"
+import { useLocation } from 'react-router-dom'
 import Logo from '../../assets/LOGO.svg'
 
 function Header() {
+    const location = useLocation()
+    const { pathname } = location
+    const splitLocation = pathname.split("/")
+
     return (
         <div className='main'>
             <header className='header'>
                 <img className='logo' src={Logo} alt='Logo du site'></img>
-                <nav className='nav'>
-                    <ul className='nav_list'>
-                        <li>
-                            <Link to="/" className='nav_list_link'>Accueil</Link>
-                        </li>
-                        <li>
-                            <Link to="/About" className='nav_list_link'>A Propos</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <ul className='nav_list'>
+                    <li className={splitLocation[1] === "" ? "active" : ""}>
+                        <Link to="/" className='nav_list_link'>Accueil</Link>
+                    </li>
+                    <li className={splitLocation[1] === "About" ? "active" : ""}>
+                        <Link to="/About" className='nav_list_link'>A Propos</Link>
+                    </li>
+                </ul>
             </header>
             <Outlet />
         </div>
